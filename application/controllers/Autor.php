a479fc002c2f1462489fdabf477e920045d93689
Jurimarek <jurimarek@gmail.com>
2018-04-16 22:34:55 +0200

    prvá tabulka, chyba vzhlad

--- application/controllers/Autor.php
+++ application/controllers/Autor.php
@@ -28,7 +28,7 @@ class Autor extends CI_Controller {
             $data['error_msg'] = $this->session->userdata('error_msg');
             $this->session->unset_userdata('error_msg');
         }
-        $data['temperatures'] = $this->autor_model->getRows();
+        $data['autor'] = $this->autor_model->getRows();
         $data['title'] = 'Zoznam autorov';
 //nahratie zoznamu teplot
         $this->load->view('templates/header', $data);
@@ -51,6 +51,96 @@ class Autor extends CI_Controller {
             redirect('/temperatures');
         }
 
+    }
+
+    // pridanie zaznamu
+    public function add(){
+        $data = array();
+        $postData = array();
+//zistenie, ci bola zaslana poziadavka na pridanie zaznamu
+        if($this->input->post('postSubmit')){
+//definicia pravidiel validacie
+            $this->form_validation->set_rules('meno_autora', 'meno_autora', 'required');
+            $this->form_validation->set_rules('priezvisko_autora', 'priezvisko_autora', 'required');
+
+            //$this->form_validation->set_rules('user', 'user id', 'required');
+//priprava dat pre vlozenie
+            $postData = array(
+                'meno_autora' => $this->input->post('meno_autora'),
+                'priezvisko_autora' => $this->input->post('priezvisko_autora'),
+            );
+//validacia zaslanych dat
+            if($this->form_validation->run() == true){
+//vlozenie dat
+                $insert = $this->autor_model->insert($postData);
+                if($insert){
+                    $this->session->set_userdata('success_msg', 'Autor bol vložený úspešne');
+                    redirect('/temperatures');
+                }else{
+                    $data['error_msg'] = 'Vyskitol sa problém, skúste znova.';
+                }
+            }
+        }
+        $data['post'] = $postData;
+        $data['title'] = 'Vytvor autora';
+
+$data['action'] = 'Add';
+//zobrazenie formulara pre vlozenie a editaciu dat
+$this->load->view('templates/header', $data);
+$this->load->view('temperatures/add-edit', $data);
+$this->load->view('templates/footer');
+}
+
+// aktualizacia dat
+    public function edit($id){
+        $data = array();
+//ziskanie dat z tabulky
+        $postData = $this->autor_model->getRows($id);
+//zistenie, ci bola zaslana poziadavka na aktualizaciu
+        if($this->input->post('postSubmit')){
+//definicia pravidiel validacie
+            $this->form_validation->set_rules('meno_autora', 'meno_autora', 'required');
+            $this->form_validation->set_rules('priezvisko_autora', 'priezvisko_autora', 'required');
+// priprava dat pre aktualizaciu
+            $postData = array(
+                'meno_autora' => $this->input->post('meno_autora'),
+                'priezvisko_autora' => $this->input->post('priezvisko_autora'),
+            );
+//validacia zaslanych dat
+            if($this->form_validation->run() == true){
+//aktualizacia dat
+                $update = $this->autor_model->update($postData, $id);
+                if($update){
+                    $this->session->set_userdata('success_msg', 'Autor bol upravený');
+
+redirect('/autori');
+}else{
+                    $data['error_msg'] = 'Vyskitol sa problém';
+                }
+            }
+        }
+        $data['post'] = $postData;
+        $data['title'] = 'Upraviť autora';
+        $data['action'] = 'Edit';
+//zobrazenie formulara pre vlozenie a editaciu dat
+        $this->load->view('templates/header', $data);
+        $this->load->view('temperatures/add-edit', $data);
+        $this->load->view('templates/footer');
+    }
+
+    // odstranenie dat
+    public function delete($id){
+//overenie, ci id nie je prazdne
+        if($id){
+//odstranenie zaznamu
+            $delete = $this->autor_model->delete($id);
+            if($delete){
+                $this->session->set_userdata('success_msg', 'Autor bol odstránený');
+            }else{
+                $this->session->set_userdata('error_msg', 'Vyskitol sa problém');
+            }
+        }
+        redirect('/autori');
     }
 
 
