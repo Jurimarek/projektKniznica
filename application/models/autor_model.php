2deb39aaabe2aab787d924b9541add5e20f53e37
Jurimarek <jurimarek@gmail.com>
2018-04-23 18:46:05 +0200

    upraveny model

--- application/models/autor_model.php
+++ application/models/autor_model.php
@@ -6,41 +6,48 @@
  * Time: 21:42
  */
 
- function getRows($id= "")
- {
-     if (!empty($id)) {
-         $query = $this->db->get_where('autor', array('id' => $id));
-         return $query->row_array();
-     } else {
-         $query = $this->db->get('autor');
-         return $query->result_array();
-     }
- }
+class autor_model extends CI_Model
+{
+
+    function getRows($id = "")
+    {
+        if (!empty($id)) {
+            $query = $this->db->get_where('autor', array('id' => $id));
+            return $query->row_array();
+        } else {
+            $query = $this->db->get('autor');
+            return $query->result_array();
+        }
+    }
 
 //vlozenie zaznamu
-public function insert($data = array()) {
-    $insert = $this->db->insert('autor', $data);
-    if($insert){
-        return $this->db->insert_id();
-    }else {
-        return false;
-    }
+    public function insert($data = array())
+    {
+        $insert = $this->db->insert('autor', $data);
+        if ($insert) {
+            return $this->db->insert_id();
+        } else {
+            return false;
+        }
 
- }
+    }
 
 // aktualizacia zaznamu
-public function update($data, $id) {
-    if(!empty($data) && !empty($id)){
-        $update = $this->db->update('autor', $data,
-            array('id'=>$id));
-        return $update?true:false;
-    }else{
-        return false;
+    public function update($data, $id)
+    {
+        if (!empty($data) && !empty($id)) {
+            $update = $this->db->update('autor', $data,
+                array('id' => $id));
+            return $update ? true : false;
+        } else {
+            return false;
+        }
     }
-}
 
 // odstranenie zaznamu
-public function delete($id){
-    $delete = $this->db->delete('autor',array('id'=>$id));
-    return $delete?true:false;
+    public function delete($id)
+    {
+        $delete = $this->db->delete('autor', array('id' => $id));
+        return $delete ? true : false;
+    }
 }
