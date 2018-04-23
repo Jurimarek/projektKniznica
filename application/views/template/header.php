b31d337b9c9afd2981d7f9e3b6fe729b5bb03c3e
Jurimarek <jurimarek@gmail.com>
2018-04-23 21:27:36 +0200

    s bootsrapom

---
+++ application/views/template/header.php
@@ -0,0 +1,9 @@
+
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+    <meta charset="utf-8">
+    <title><?php echo $title; ?></title>
+    <link href="<?php echo base_url();?>assets/css/bootstrap.min.css" rel="stylesheet"> <!-- Bootstrap theme --> <link href="<?php echo base_url();?>
+</head>
+<body>
